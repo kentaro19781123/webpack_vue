79e2b1938edae2f7d82d9539c4f00badb2df11da
suzuki kentaro <suzuki-ke@gnavi.co.jp>
2019-10-03 18:50:02 +0900

    webpackconfig変更

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const globule = require('globule')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
@@ -7,14 +8,31 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
-module.exports = {
+const rootPath = path.resolve(__dirname, '../')
+const targetTypes = { pug : 'html'}
+
+const entriesList = {};
+const getEntriesList = () => {
+  for(const [ srcType, targetType ] of Object.entries(targetTypes)) {
+    const filesMatched = globule.find([`**/*.${srcType}`, `!**/_*.${srcType}`], { cwd : `${rootPath}/src` });
+
+    for(const srcName of filesMatched) {
+      const targetPathName = srcName.replace(new RegExp(`.${srcType}$`, 'i'), `.${targetType}`);
+      const targetName = targetPathName.replace('page/', '');
+      entriesList[targetName] = `${rootPath}/src/${srcName}`;
+    }
+  }
+  return entriesList;
+}
+
+const config = {
   entry: {
     script: './src/js/script.js',
     main: './src/css/style.css'
   },
   output: {
     filename: 'js/[name].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, '../dist')
   },
   resolve: {
     extensions: ['.js', '.vue'],
@@ -117,14 +135,14 @@ module.exports = {
       from: './src/img',
       to: 'img'
     }]),
-    new HtmlWebPackPlugin({
-      template: './src/page/index.pug',
-      filename: './index.html'
-    }),
-    new HtmlWebPackPlugin({
-      template: './src/page/content/index.pug',
-      filename: './content/index.html'
-    }),
+    // new HtmlWebPackPlugin({
+    //   template: './src/page/index.pug',
+    //   filename: './index.html'
+    // }),
+    // new HtmlWebPackPlugin({
+    //   template: './src/page/content/index.pug',
+    //   filename: './content/index.html'
+    // }),
     new FixStyleOnlyEntriesPlugin(),
     new MiniCssExtractPlugin({
       filename: './css/[name].css'
@@ -135,3 +153,12 @@ module.exports = {
     minimizer: [new OptimizeCSSAssetsPlugin({})]
   }
 }
+
+for(const [ targetName, srcName ] of Object.entries(getEntriesList())) {
+  config.plugins.push(new HtmlWebPackPlugin({
+    filename : targetName,
+    template : srcName
+  }));
+}
+
+module.exports = config
